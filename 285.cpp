7d4be3aa5e44da1df8d2932c45c6a08ea3c31b56
hsiangkuotang <hsiangkuo.tang@gmail.com>
2015-10-16 19:13:31 -0700

    Create 285.cpp

---
+++ 285.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        if (root == NULL) return NULL;
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+        while(curr) {
+            st.push(curr);
+            curr = curr->left;
+        }
+        while (!st.empty()) {
+            TreeNode* node = st.top();
+            st.pop();
+            if (node->right) {
+                curr = node->right;
+                while(curr) {
+                    st.push(curr);
+                    curr = curr->left;
+                }
+            }
+            if (node == p && !st.empty()) return st.top();
+        }
+        return NULL;
+    }
+};
