5ae23e009b12a8e27548769f92327446660240bc
hsiangkuotang <hsiangkuo.tang@gmail.com>
2015-10-16 19:20:13 -0700

    Create 280.cpp

---
+++ 280.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); i++) {
+            if ((i % 2 == 1 && nums[i] < nums[i - 1]) || (i % 2 == 0 && nums[i] > nums[i - 1])) {
+                int tmp = nums[i - 1];
+                nums[i - 1] = nums[i];
+                nums[i] = tmp;
+            }
+        }
+    }
+};
