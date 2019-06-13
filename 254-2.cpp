9b8af9d9c60579dd81c0a001f6cc488b23930d84
hktwarzone <hsiangkuo.tang@gmail.com>
2019-06-13 16:27:20 -0700

    Update 254-2.cpp

--- 254-2.cpp
+++ 254-2.cpp
@@ -1,3 +1,25 @@
+/*
+Factor Combinations
+umbers can be regarded as product of its factors. For example,
+8 = 2 x 2 x 2;
+  = 2 x 4.
+Write a function that takes an integer n and return all possible combinations of its factors.
+Note: 
+Each combination's factors must be sorted ascending, for example: The factors of 2 and 6 is [2, 6], not [6, 2].
+You may assume that n is always positive.
+Factors should be greater than 1 and less than n.
+input: 32
+output:
+[
+  [2, 16],
+  [2, 2, 8],
+  [2, 2, 2, 4],
+  [2, 2, 2, 2, 2],
+  [2, 4, 4],
+  [4, 8]
+]
+*/
+
 class Solution {
 public:
     /**
